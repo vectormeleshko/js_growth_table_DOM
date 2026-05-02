c1416a8c47bea24fef2104437f7721479a4acadc
Viktor Meleshko <vectormeleshko@gmail.com>
2026-05-02 10:44:34 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendCol = document.querySelector('.append-column');
+const removeCol = document.querySelector('.remove-column');
+const table = document.querySelector('table');
+
+function checkTable() {
+  const rowsCount = table.rows.length;
+  const colsCount = table.rows[0].cells.length;
+
+  appendRow.disabled = rowsCount === 10;
+  removeRow.disabled = rowsCount === 2;
+
+  appendCol.disabled = colsCount === 10;
+  removeCol.disabled = colsCount === 2;
+}
+
+appendRow.addEventListener('click', () => {
+  const newRow = table.insertRow();
+  for (let i = 0; i < table.rows[0].cells.length; i++) {
+    newRow.insertCell();
+  }
+  checkTable();
+});
+ removeRow.addEventListener('click', () => {
+  table.deleteRow(-1);
+  checkTable();
+});
+
+appendCol.addEventListener('click', () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].insertCell();
+  }
+  checkTable();
+});
+
+removeCol.addEventListener('click', () => {
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].deleteCell(-1);
+  }
+  checkTable();
+});
+
+checkTable();
+
